ca1e96d857da0996eb77e488e115ef9e225c4a50
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-12-14 16:19:31 -0500

    hack at types....

--- types/global.d.ts
+++ types/global.d.ts
@@ -5,3 +5,16 @@ declare module 'ember-lifecycle-component/templates/*' {
   export default tmpl;
 }
 
+declare module '@ember/component' {
+  export function setComponentManager<T>(managerId: string, baseClass: T): T;
+  export function setComponentManager<T>(managerFactory: (owner: any) => {}, baseClass: T): T;
+  export function capabilities(
+    version: string,
+    opts?: {
+      destructor?: boolean;
+      asyncLifecycleCallbacks?: boolean;
+      updateHook?: boolean;
+    }
+  ): any;
+}
+
